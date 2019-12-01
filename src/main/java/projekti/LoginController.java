0e78975b992a6388c4657c97e740de4754579975
mikaelrauhala <mikael.rauhala@eduespoo.fi>
2019-12-01 13:02:22 +0200

    can add pics to profile

---
+++ src/main/java/projekti/LoginController.java
@@ -0,0 +1,4 @@
+package projekti;
+
+public class LoginController {
+}
---
+++ src/main/java/projekti/RegistrationController.java
@@ -0,0 +1,18 @@
+package projekti;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+
+
+
+}
