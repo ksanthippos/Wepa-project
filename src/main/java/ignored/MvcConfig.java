1b75882b6256abd44e3ea31d2441284fcb56187f
mikaelrauhala <mikael.rauhala@eduespoo.fi>
2019-12-13 11:49:19 +0200

    heroku version 4

--- src/main/java/ignored/MvcConfig.java
+++ src/main/java/ignored/MvcConfig.java
@@ -1,4 +1,4 @@
-package projekti.security;
+package ignored;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
